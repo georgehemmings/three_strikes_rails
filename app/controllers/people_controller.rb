301e8373a72666f494970f319607370f2b34f455
George Hemmings <gh@georgehemmings.com>
2012-06-25 22:00:14 +0100

    Put REST actions in scaffold order

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,25 +2,25 @@ class PeopleController < ApplicationController
 
   respond_to :json
 
-  def create
-    person = Person.create(params[:person])
-    respond_with person
-  end
-
   def show
     person = Person.find(params[:id])
     respond_with person
   end
 
-  def destroy
-    person = Person.find(params[:id])
-    person.destroy
+  def create
+    person = Person.create(params[:person])
     respond_with person
   end
   
   def update
     person = Person.find(params[:id])
     person.update_attributes(params[:person])
+    respond_with person
+  end
+  
+  def destroy
+    person = Person.find(params[:id])
+    person.destroy
     respond_with person
   end
 end
--- app/controllers/strikes_controller.rb
+++ app/controllers/strikes_controller.rb
@@ -7,15 +7,15 @@ class StrikesController < ApplicationController
     respond_with strike
   end
 
-  def destroy
-    strike = Strike.find(params[:id])
-    strike.destroy
-    respond_with strike
-  end
-
   def update
     strike = Strike.find(params[:id])
     strike.update_attributes(params[:strike])
+    respond_with strike
+  end
+
+  def destroy
+    strike = Strike.find(params[:id])
+    strike.destroy
     respond_with strike
   end
 end
